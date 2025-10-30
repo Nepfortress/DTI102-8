b2fd30d3e86b4be92f220ddf35d2793e3e521bbb
Nepfortress <nepfortress2006@gmail.com>
2025-10-30 08:50:03 +0700

    Add files via upload

--- background.py
+++ background.py
@@ -1,39 +1,8 @@
 import pygame
 
-bg = pygame.image.load("magicimg/softmoutain.png")
-def backgroundImg(bg):
-    size = pygame.transform.scale(bg, (1920, 1080))
-    screen.blit(size, (0, 0))
-
-# Merge this later
-
-"""
 # madebyverycuteperson
-import pygame
-pygame.init() 
-pygame.display.set_caption("Magic type") 
 
-WHITE =(255,255,255)
-RED =(255,0,0)
-GREEN =(0,255,0)
-
-
-screen = pygame.display.set_mode((1920,1080))
-# screen.fill(WHITE)
-
-softmoutain=pygame.image.load("magicimg/softmoutain.png")
-softmoutain= pygame.transform.scale(softmoutain,(1920,1080))
-
-# pygame.draw.rect(screen,WHITE,(80,100,250,50))
-
-
-running = True
-while running:
-    for event in pygame.event.get():
-     if event.type == pygame.QUIT:
-        running==False
-    
-    screen.blit(softmoutain,(0,0))
-    pygame.display.update()
-pygame.quit()
-"""
+softmountain = pygame.image.load("softmountain.png")
+def backgroundImg(softmountain):
+    softmountain = pygame.transform.scale(softmountain, (1920, 1080))
+    pygame.screen.blit(softmountain, (0, 0))
